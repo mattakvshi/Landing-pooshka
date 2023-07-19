422bd09ce112846b72563a907d0aa3aaa8435e6c
mattakvshi <ytube9504@gmail.com>
2023-07-19 23:25:50 +0300

    Bug fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,13 @@
 import Navbar from './../components/navbar/Navbar';
 
 const About = () => {
+	alert(
+		'ДАННАЯ СТРАНИЦА НАХОДИТСЯ В СТАДИИ РАЗРАБОТКИ!!! ПОДДЕРЖИТЕ МОЛОДЫХ РАЗРАБОТЧИКОВ ПЖ: СБЕР - +7 (918) 284-28-48.'
+	);
 	return (
 		<>
 			<Navbar />
-			<section className='about'></section>;
+			<section className='about'></section>
 		</>
 	);
 };
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,9 @@
 import Navbar from './../components/navbar/Navbar';
 
 const Products = () => {
+	alert(
+		'ДАННАЯ СТРАНИЦА НАХОДИТСЯ В СТАДИИ РАЗРАБОТКИ!!! ПОДДЕРЖИТЕ МОЛОДЫХ РАЗРАБОТЧИКОВ ПЖ: СБЕР - +7 (918) 284-28-48.'
+	);
 	return (
 		<>
 			<Navbar />
