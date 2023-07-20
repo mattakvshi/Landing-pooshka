a3852bc3fa3ab099b9bfb51f1795cb1cfafd3bc0
Alex <Sanchous557@mail.ru>
2023-07-20 15:40:58 +0300

    bug fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,105 +1,105 @@
-import './footer.css';
+import "./footer.css";
 
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import footerLogo from './../../img/footer/logo-footer-white.png';
+import footerLogo from "./../../img/footer/logo-footer-white.png";
 
-import footerIconMail from './../../img/footer/mail-icon.svg';
-import footerIconPhone from './../../img/footer/phone-icon.svg';
-import footerIconTelegram from './../../img/footer/telegram-icon.png';
+import footerIconMail from "./../../img/footer/mail-icon.svg";
+import footerIconPhone from "./../../img/footer/phone-icon.svg";
+import footerIconTelegram from "./../../img/footer/telegram-icon.png";
 
 const Footer = () => {
-	return (
-		<footer className='footer'>
-			<div className='waves'>
-				<div className='wave' id='wave1'></div>
-				<div className='wave' id='wave2'></div>
-				<div className='wave' id='wave3'></div>
-				<div className='wave' id='wave4'></div>
-			</div>
-			<div className='container-footer'>
-				<div className='footer-wrapper'>
-					<div className='footer-col'>
-						<NavLink to='/'>
-							<img
-								className='logo-footer'
-								src={footerLogo}
-								alt='footer-logo'
-								width='180px'
-								height='40px'
-							/>
-						</NavLink>
+  return (
+    <footer className="footer">
+      <div className="waves">
+        <div className="wave" id="wave1"></div>
+        <div className="wave" id="wave2"></div>
+        <div className="wave" id="wave3"></div>
+        <div className="wave" id="wave4"></div>
+      </div>
+      <div className="container-footer">
+        <div className="footer-wrapper">
+          <div className="footer-col">
+            <NavLink to="/Landing-pooshka/">
+              <img
+                className="logo-footer"
+                src={footerLogo}
+                alt="footer-logo"
+                width="180px"
+                height="40px"
+              />
+            </NavLink>
 
-						<div className='footer-icon-block'>
-							<img
-								className='footer-mail-icon'
-								alt='footer-mail-icon'
-								src={footerIconMail}
-							/>
-							<p>help@frybix.com</p>
-						</div>
-						<div className='footer-icon-block'>
-							<img
-								className='footer-phone-icon'
-								alt='footer-phone-icon'
-								src={footerIconPhone}
-							/>
-							<p>+1 234 456 678 89</p>
-						</div>
-						<div className='footer-icon-block'>
-							<img
-								className='footer-telegram-icon'
-								alt='footer-telegram-icon'
-								src={footerIconTelegram}
-							/>
-							<p>Наш телеграм</p>
-						</div>
-					</div>
-					<div className='footer-col'>
-						<h3 className='title-3-footer'>Ссылки</h3>
-						<NavLink to='/' className='footer-link-text'>
-							Главная
-						</NavLink>
-						<NavLink to='/products' className='footer-link-text'>
-							Товары
-						</NavLink>
-						<NavLink to='/about' className='footer-link-text'>
-							О нас
-						</NavLink>
-						<NavLink to='/contacts' className='footer-link-text'>
-							Контакты
-						</NavLink>
-					</div>
-					<div className='footer-col'>
-						<h3 className='title-3-footer'>Соглашения</h3>
-						<a href='#!' className='footer-link-text'>
-							Пользовательское
-						</a>
-						<a href='#!' className='footer-link-text'>
-							Конфиденциальность
-						</a>
-						<a href='#!' className='footer-link-text'>
-							Файлы куки
-						</a>
-					</div>
-					<div className='footer-col'>
-						<h3 className='title-3-footer'>Продукция</h3>
-						<a href='#!' className='footer-link-text'>
-							Посмотреть товары
-						</a>
-						<a href='#!' className='footer-link-text'>
-							Задать вопрос
-						</a>
-						<a href='#!' className='footer-link-text'>
-							Отзывы
-						</a>
-					</div>
-				</div>
-				<div className='footer-dev'>
-					<p>Created in 2023 by mattakushi & lumate </p>
-				</div>
-			</div>
-		</footer>
-	);
+            <div className="footer-icon-block">
+              <img
+                className="footer-mail-icon"
+                alt="footer-mail-icon"
+                src={footerIconMail}
+              />
+              <p>help@frybix.com</p>
+            </div>
+            <div className="footer-icon-block">
+              <img
+                className="footer-phone-icon"
+                alt="footer-phone-icon"
+                src={footerIconPhone}
+              />
+              <p>+1 234 456 678 89</p>
+            </div>
+            <div className="footer-icon-block">
+              <img
+                className="footer-telegram-icon"
+                alt="footer-telegram-icon"
+                src={footerIconTelegram}
+              />
+              <p>Наш телеграм</p>
+            </div>
+          </div>
+          <div className="footer-col">
+            <h3 className="title-3-footer">Ссылки</h3>
+            <NavLink to="/" className="footer-link-text">
+              Главная
+            </NavLink>
+            <NavLink to="/products" className="footer-link-text">
+              Товары
+            </NavLink>
+            <NavLink to="/about" className="footer-link-text">
+              О нас
+            </NavLink>
+            <NavLink to="/contacts" className="footer-link-text">
+              Контакты
+            </NavLink>
+          </div>
+          <div className="footer-col">
+            <h3 className="title-3-footer">Соглашения</h3>
+            <a href="#!" className="footer-link-text">
+              Пользовательское
+            </a>
+            <a href="#!" className="footer-link-text">
+              Конфиденциальность
+            </a>
+            <a href="#!" className="footer-link-text">
+              Файлы куки
+            </a>
+          </div>
+          <div className="footer-col">
+            <h3 className="title-3-footer">Продукция</h3>
+            <a href="#!" className="footer-link-text">
+              Посмотреть товары
+            </a>
+            <a href="#!" className="footer-link-text">
+              Задать вопрос
+            </a>
+            <a href="#!" className="footer-link-text">
+              Отзывы
+            </a>
+          </div>
+        </div>
+        <div className="footer-dev">
+          <p>Created in 2023 by mattakushi & lumate </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,77 +1,77 @@
-import './navbar.css';
+import "./navbar.css";
 
-import logo from './../../img/headerNav/logo-Pooshka-white.png';
+import logo from "./../../img/headerNav/logo-Pooshka-white.png";
 
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-	const activeLink = 'nav-list__link nav-list__link--active';
-	const normalLik = 'nav-list__link';
+  const activeLink = "nav-list__link nav-list__link--active";
+  const normalLik = "nav-list__link";
 
-	const normalNav = 'nav';
-	const otherNan = 'other-nav';
-	var navClass;
+  const normalNav = "nav";
+  const otherNan = "other-nav";
+  var navClass;
 
-	const activeURL = window.location.pathname;
-	if (activeURL === '/') {
-		navClass = normalNav;
-	} else {
-		navClass = otherNan;
-	}
+  const activeURL = window.location.pathname;
+  if (activeURL === "/Landing-pooshka/") {
+    navClass = normalNav;
+  } else {
+    navClass = otherNan;
+  }
 
-	return (
-		<nav className={navClass}>
-			<div className='container-nav'>
-				<div className='nav-row'>
-					<NavLink to='/' className='logo'>
-						<img src={logo} alt='logo' width='250px' height='60px' />
-					</NavLink>
-					<ul className='nav-list'>
-						<li className='nav-list__item'>
-							<NavLink
-								to='/'
-								className={({ isActive }) => {
-									return isActive ? activeLink : normalLik;
-								}}
-							>
-								Главная
-							</NavLink>
-						</li>
-						<li className='nav-list__item'>
-							<NavLink
-								to='/products'
-								className={({ isActive }) =>
-									isActive ? activeLink : normalLik
-								}
-							>
-								Товары
-							</NavLink>
-						</li>
-						<li className='nav-list__item'>
-							<NavLink
-								to='/about'
-								className={({ isActive }) =>
-									isActive ? activeLink : normalLik
-								}
-							>
-								О нас
-							</NavLink>
-						</li>
-						<li className='nav-list__item'>
-							<NavLink
-								to='/contacts'
-								className={({ isActive }) =>
-									isActive ? activeLink : normalLik
-								}
-							>
-								Контакты
-							</NavLink>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className={navClass}>
+      <div className="container-nav">
+        <div className="nav-row">
+          <NavLink to="/Landing-pooshka/" className="logo">
+            <img src={logo} alt="logo" width="250px" height="60px" />
+          </NavLink>
+          <ul className="nav-list">
+            <li className="nav-list__item">
+              <NavLink
+                to="/Landing-pooshka/"
+                className={({ isActive }) => {
+                  return isActive ? activeLink : normalLik;
+                }}
+              >
+                Главная
+              </NavLink>
+            </li>
+            <li className="nav-list__item">
+              <NavLink
+                to="/Landing-pooshka/products"
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLik
+                }
+              >
+                Товары
+              </NavLink>
+            </li>
+            <li className="nav-list__item">
+              <NavLink
+                to="/Landing-pooshka/about"
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLik
+                }
+              >
+                О нас
+              </NavLink>
+            </li>
+            <li className="nav-list__item">
+              <NavLink
+                to="/Landing-pooshka/contacts"
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLik
+                }
+              >
+                Контакты
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
