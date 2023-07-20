74614748792784bd4f61077b7f3a8a54911f4e6f
Alex <Sanchous557@mail.ru>
2023-07-20 15:32:22 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import './style/main.css';
+import "./style/main.css";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Products from './pages/Products';
-import Contacts from './pages/Contacts';
-import Footer from './components/footer/Footer';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Contacts from "./pages/Contacts";
+import Footer from "./components/footer/Footer";
 
 function App() {
-	return (
-		<div className='App'>
-			<Router>
-				{/* <!----------------------------PAGES----------------------------------------> */}
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/products' element={<Products />} />
-					<Route path='/contacts' element={<Contacts />} />
-				</Routes>
-				{/* <!----------------------------PAGES----------------------------------------> */}
-				{/* <!----------------------------FOOTER----------------------------------------> */}
-				<Footer />
-				{/* <!----------------------------FOOTER----------------------------------------> */}
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        {/* <!----------------------------PAGES----------------------------------------> */}
+        <Routes>
+          <Route path={["/", "/home"]} element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        {/* <!----------------------------PAGES----------------------------------------> */}
+        {/* <!----------------------------FOOTER----------------------------------------> */}
+        <Footer />
+        {/* <!----------------------------FOOTER----------------------------------------> */}
+      </Router>
+    </div>
+  );
 }
 
 export default App;
