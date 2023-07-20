2c724281acf8233f837f4878e372717e0aa38be6
Alex <Sanchous557@mail.ru>
2023-07-21 00:51:44 +0300

    add page about and slider.aboba

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./slider.css";
+import ArrowButton from "./../arrowButton/ArrowButton";
+
+const Slider = ({ children }) => {
+  const [activeId, setActiveId] = useState(0);
+
+  const prev = () => {
+    setActiveId((activeId) => {
+      if (activeId > 0) {
+        return activeId - 1;
+      }
+      return activeId;
+    });
+  };
+
+  const next = () => {
+    setActiveId((activeId) => {
+      if (activeId < children.length - 1) {
+        return activeId + 1;
+      }
+      return activeId;
+    });
+  };
+  return (
+    <div className="slider-wrapper">
+      <div className="slider-actions">
+        {children.map((slide, idx) => (
+          <div className={`slide${idx === activeId ? " active" : ""}`}>
+            {slide}
+          </div>
+        ))}
+      </div>
+      <ArrowButton direction="left" onClick={prev} disable={activeId === 0} />
+      <ArrowButton
+        direction="right"
+        onClick={next}
+        disable={activeId === children.length - 1}
+      />
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,38 @@
-import Navbar from './../components/navbar/Navbar';
+import Navbar from "./../components/navbar/Navbar";
+
+import Slider from "./../components/Slider/Slider";
+import slider1 from "./../img/about_slider/slider1.png";
+import slider2 from "./../img/about_slider/slider2.png";
+import slider3 from "./../img/about_slider/slider3.png";
 
 const About = () => {
-	alert(
-		'ДАННАЯ СТРАНИЦА НАХОДИТСЯ В СТАДИИ РАЗРАБОТКИ!!! ПОДДЕРЖИТЕ МОЛОДЫХ РАЗРАБОТЧИКОВ ПЖ: СБЕР - +7 (918) 284-28-48.'
-	);
-	return (
-		<>
-			<Navbar />
-			<section className='about'></section>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <section className="section-about-info">
+        <div className="container">
+          <div className="info-wrapper">
+            <h1 className="title-1">Немного о нашей компании</h1>
+            <p>
+              Тут будет какой то текст о нашей компании
+              рвафоынпшЦУШЖЦ4ШШКРЦгарвардывраолцацФУРОФЫВРДЛОАПРЛ
+            </p>
+          </div>
+          <div className="gallery-wrapper">
+            <h2 className="title-2">Наша галерея</h2>
+
+            <div className="gallery">
+              <Slider>
+                <img src={slider1} alt="sliderImage1" />
+                <img src={slider2} alt="sliderImage2" />
+                <img src={slider3} alt="sliderImage3" />
+              </Slider>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default About;
